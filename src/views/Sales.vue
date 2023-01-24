2dc20bde82bd204403237e2c9335981ac784506d
KaelNierras <kyleanthony47@gmail.com>
2023-01-24 13:27:40 +0800

    Update Sales.vue

--- src/views/Sales.vue
+++ src/views/Sales.vue
@@ -1,11 +1,11 @@
 <script setup>
-import tables from '../components/Table.vue'
+import table from '../components/Table.vue'
 </script>
 
 <template>
     <main class="sales-page">
         <h1>Summary of Sale 2023</h1>
-        <tables/>
+        <table/>
     </main>
 </template>
 
